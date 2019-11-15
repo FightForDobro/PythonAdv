36eb3b30d8c0076b145bf3d22c2c6ea7f45b45df
Denys <FightForDobro@gmail.com>
2019-11-15 21:56:26 +0200

    fix2

--- MainProject/config.py
+++ MainProject/config.py
@@ -1,6 +1,6 @@
 TOKEN = '1016303038:AAF4ENbETYZQak6PHYfSA8hDFb8xLGwyzaQ'
 HOST = '127.0.0.1'
-WEBHOOK_URL = f'https://35.204.193.143/{TOKEN[::-1]}'  # Set webhook for this url
+WEBHOOK_URL = f'https://35.204.193.143/'  # Set webhook for this url
 HANDEL_URL = f'/'
 
 
